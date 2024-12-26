c3407ed1ea910c4b8a165f4ccb028c9adc8bbe29
Ilya <karakulinilia@gmail.com>
2024-12-26 13:12:55 +0700

    del comment

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 int main()
 {
     std::ifstream file("./production.csv");
-    // 5005072170100, 2016, 10, 3835,11281, 2.9415906127770497,188,0.0490221642764016,31,2016-12-22 14:18:34.197
     CSVReader<long long, int, int, int, int, double, int, double, int, std::string> parser(file, 1, '\n', ',');
 
     for (auto it = parser.begin(), end = parser.end(); it != end; ++it)
